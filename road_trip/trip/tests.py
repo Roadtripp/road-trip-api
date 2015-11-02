0edb07b7419d69f75ca4f2504de361a440d5e5b4
Jonathan Frederick <jrfreder@gmail.com>
2015-11-02 13:48:13 -0500

    wrote test for trip creation

--- road_trip/trip/tests.py
+++ road_trip/trip/tests.py
@@ -4,4 +4,19 @@ from .models import *
 
 # Create your tests here.
 class TripTestCase(TestCase):
-    pass
+    def setUp(self):
+        test_trip = Trip.objects.create(
+            pk=1,
+            title="TITLE",
+            origin="800 Blackwell St. Durham, NC",
+            destination="New York, NY",
+            start="2008-04-10 11:47:58-05",
+            end="2008-04-13 11:00:00-05",
+        )
+
+    def test_trip_creation(self):
+        self.assertEquals(Trip.objects.get(title="TITLE").pk, 1),
+        self.assertEquals(Trip.objects.get(origin="800 Blackwell St. Durham, NC").pk, 1),
+        self.assertEquals(Trip.objects.get(destination="New York, NY").pk, 1),
+        self.assertEquals(Trip.objects.get(start="2008-04-10 11:47:58-05").pk, 1),
+        self.assertEquals(Trip.objects.get(end="2008-04-13 11:00:00-05").pk, 1),
