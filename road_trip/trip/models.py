aaeddeaa27c9088ec6a492a2175bba1a88ab7caa
Jonathan Frederick <jrfreder@gmail.com>
2015-11-02 14:27:46 -0500

    initial Trip, not tested

--- road_trip/trip/models.py
+++ road_trip/trip/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Trip(models.Model):
+    title = models.CharField(max_length=255)
+    origin = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
