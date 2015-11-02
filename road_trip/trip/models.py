72838f503e9bded8a925a59ee66f2f3a66657374
Jonathan Frederick <jrfreder@gmail.com>
2015-11-02 15:03:40 -0500

    added city model

--- road_trip/trip/models.py
+++ road_trip/trip/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+
 class Trip(models.Model):
     title = models.CharField(max_length=255)
     origin = models.CharField(max_length=255)
     destination = models.CharField(max_length=255)
     start = models.DateTimeField()
     end = models.DateTimeField()
+
+
+class City(models.Model):
+    city_name = models.CharField(max_length=255)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    trip = models.ForeignKey(Trip)
