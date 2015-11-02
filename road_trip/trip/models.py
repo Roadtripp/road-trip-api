6b80081df8fcd7ab7e8c59fdf2d4f06809b08c71
Jonathan Frederick <jrfreder@gmail.com>
2015-11-02 18:52:53 -0500

    new trip creation test is passing

--- road_trip/trip/models.py
+++ road_trip/trip/models.py
@@ -6,9 +6,15 @@ from django.db import models
 class Trip(models.Model):
     title = models.CharField(max_length=255)
     origin = models.CharField(max_length=255)
+    origin_date = models.DateField(null=True, blank=True)
+    origin_time = models.TimeField(null=True, blank=True)
+    origin_lat = models.FloatField(default=None)
+    origin_lon = models.FloatField(default=None)
     destination = models.CharField(max_length=255)
-    start = models.DateTimeField()
-    end = models.DateTimeField()
+    destination_date = models.DateField(null=True, blank=True)
+    destination_time = models.TimeField(null=True, blank=True)
+    destination_lat = models.FloatField(default=None)
+    destination_lon = models.FloatField(default=None)
 
 
 class City(models.Model):
