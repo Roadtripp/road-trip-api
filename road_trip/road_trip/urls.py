1d0531be1e319eb68c3f82931e6a79ddefcb7f3f
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 10:54:34 -0500

    changed routers import location

--- road_trip/road_trip/urls.py
+++ road_trip/road_trip/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers
+from rest_framework_nested import routers
 from trip.views import TripViewSet
 
 router = routers.DefaultRouter()
