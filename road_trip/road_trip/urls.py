67c3458899117dad90dddd514802a40435669021
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 10:03:35 -0500

    added urls for api-auth and swagger docs

--- road_trip/road_trip/urls.py
+++ road_trip/road_trip/urls.py
@@ -24,4 +24,7 @@ router.register(r'trip', TripViewSet)
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
