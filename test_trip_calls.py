99d678e0b9bd8678d9339772ce95ce935a29e119
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 01:51:40 -0500

    testing trip patch, get, delete

--- test_trip_calls.py
+++ test_trip_calls.py
@@ -10,25 +10,58 @@ def smoke_test():
 
 
 def trip_post_test():
-    head = {'title':'TITLE', 'origin':'334 Blackwell Street B017, Durham, NC',
+    head_post = {'title':'TITLE',
+    'origin':'334 Blackwell Street B017, Durham, NC',
     'origin_date':'08/25/2004', 'origin_time':'12:00 PM',
     'destination':'New York, NY', 'destination_date':'08/28/2004',
     'destination_time':'12:00 PM'}
     url = 'http://127.0.0.1:8000/api/trip/'
-    res_post = requests.post(url, data=head)
-    j = json.loads(res_post.text)
+    res_post = requests.post(url, data=head_post)
+    j_post = json.loads(res_post.text)
 
-    assert j['title'] == 'TITLE'
-    assert j['title'] != 'TITLE2'
-    assert j['origin'] == '334 Blackwell Street B017, Durham, NC'
-    assert j['origin'] != '335 Blackwell Street B017, Durham, NC'
-    assert j['destination'] == 'New York, NY'
-    assert j['destination'] != 'Miami, FL'
-    assert j['origin_time'] == '12:00:00'
-    assert j['origin_time'] != '12:00:01'
-    assert j['origin_date'] =='08/25/2004'
-    assert j['origin_date'] !='08/25/2005'
-    assert j['destination_date'] == '08/28/2004'
-    assert j['destination_date'] != '08/29/2004'
-    assert j['destination_time'] == '12:00:00'
-    assert j['destination_time'] != '12:01:00'
+    assert res_post.status_code == 201
+    assert j_post['title'] == 'TITLE'
+    assert j_post['title'] != 'TITLE2'
+    assert j_post['origin'] == '334 Blackwell Street B017, Durham, NC'
+    assert j_post['origin'] != '335 Blackwell Street B017, Durham, NC'
+    assert j_post['destination'] == 'New York, NY'
+    assert j_post['destination'] != 'Miami, FL'
+    assert j_post['origin_time'] == '12:00:00'
+    assert j_post['origin_time'] != '12:00:01'
+    assert j_post['origin_date'] =='08/25/2004'
+    assert j_post['origin_date'] !='08/25/2005'
+    assert j_post['destination_date'] == '08/28/2004'
+    assert j_post['destination_date'] != '08/29/2004'
+    assert j_post['destination_time'] == '12:00:00'
+    assert j_post['destination_time'] != '12:01:00'
+
+    head_patch = {'title':'TITLE2'}
+    res_patch = requests.patch(url+str(j_post['id'])+'/', data=head_patch)
+    j_patch = json.loads(res_patch.text)
+
+    assert res_patch.status_code == 200
+    assert j_patch['title'] != 'TITLE'
+    assert j_patch['title'] == 'TITLE2'
+
+    res_get = requests.get(url+str(j_post['id'])+'/')
+    j_get = json.loads(res_get.text)
+
+    assert res_get.status_code == 200
+    assert j_get['title'] == 'TITLE2'
+    assert j_get['title'] != 'TITLE'
+    assert j_get['origin'] == '334 Blackwell Street B017, Durham, NC'
+    assert j_get['origin'] != '335 Blackwell Street B017, Durham, NC'
+    assert j_get['destination'] == 'New York, NY'
+    assert j_get['destination'] != 'Miami, FL'
+    assert j_get['origin_time'] == '12:00:00'
+    assert j_get['origin_time'] != '12:00:01'
+    assert j_get['origin_date'] =='08/25/2004'
+    assert j_get['origin_date'] !='08/25/2005'
+    assert j_get['destination_date'] == '08/28/2004'
+    assert j_get['destination_date'] != '08/29/2004'
+    assert j_get['destination_time'] == '12:00:00'
+    assert j_get['destination_time'] != '12:01:00'
+
+    res_delete = requests.delete(url+str(j_post['id'])+'/')
+
+    assert res_delete.status_code == 204
