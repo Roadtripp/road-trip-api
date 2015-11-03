9b63e89d2599695514f3a07f69bf6c4583dc4549
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 13:35:49 -0500

    passing city api tests

--- road_trip/trip/views.py
+++ road_trip/trip/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from .models import Trip, City
 from .serializers import TripSerializer, CitySerializer
@@ -14,3 +14,13 @@ class TripViewSet(viewsets.ModelViewSet):
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
+    def get_queryset(self):
+        trip_pk = self.kwargs['trip_pk']
+        tr = get_object_or_404(Trip, pk=trip_pk)
+        return self.queryset.filter(trip=tr)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context().copy()
+        context['trip_pk'] = self.kwargs['trip_pk']
+        return context
