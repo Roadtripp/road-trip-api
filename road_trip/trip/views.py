6a14a5dd0d453f1d1f6ea808627f423e5ba6a490
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 11:57:35 -0500

    getting to CitySerializer

--- road_trip/trip/views.py
+++ road_trip/trip/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Trip
-from .serializers import TripSerializer
+from .models import Trip, City
+from .serializers import TripSerializer, CitySerializer
 
 # Create your views here.
+
 
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
