400ab0071d8a3f15e37d3e1c3766feba3acb68c3
Jonathan Frederick <jrfreder@gmail.com>
2015-11-03 10:49:55 -0500

    wrote tests for city api

--- test_trip_calls.py
+++ test_trip_calls.py
@@ -4,12 +4,16 @@ import json
 
 
 def smoke_test():
+    """Test to make sure nosetests works"""
     assert True
 
 # TODO: figure out how to run and kill server in file
 
 
 def trip_api_test():
+    """Testing suite for Trip API calls
+       (POST, PATCH, GET, DELETE)"""
+
     head_post = {'title': 'TITLE',
                  'origin': '334 Blackwell Street B017, Durham, NC',
                  'origin_date': '08/25/2004', 'origin_time': '12:00 PM',
@@ -69,18 +73,53 @@ def trip_api_test():
 
 
 def city_api_test():
+    """Testing suite for City API calls
+       (POST, PATCH, GET, DELETE)"""
+    # POST a trip
     head_trip_post = {'title': 'TITLE',
                       'origin': '334 Blackwell Street B017, Durham, NC',
                       'origin_date': '08/25/2004', 'origin_time': '12:00 PM',
                       'destination': 'New York, NY',
                       'destination_date': '08/28/2004',
                       'destination_time': '12:00 PM'}
-    url = 'http://127.0.0.1:8000/api/trip/'
-    res_trip_post = requests.post(url, data=head_trip_post)
+    trip_url = 'http://127.0.0.1:8000/api/trip/'
+    res_trip_post = requests.post(trip_url, data=head_trip_post)
     j_trip_post = json.loads(res_trip_post.text)
-    url += str(j_trip_post['id']) + '/'
+    trip_url += str(j_trip_post['id']) + '/'
 
+    # POST a city
+    head_city_post = {'city_name': 'Boston, MA', 'trip': j_trip_post['id']}
+    res_city_post = requests.post(trip_url+'city/', data=head_city_post)
+    j_city_post = json.loads(res_city_post.text)
+    city_url = trip_url+'city/' + str(j_city_post['id']) + '/'
 
+    assert res_city_post.status_code == 201
+    assert j_city_post['city_name'] == 'Boston, MA'
+    assert j_city_post['trip'] == j_trip_post['id']
 
-    res_delete = requests.delete(url)
+    # PATCH a city
+    head_city_patch = {'city_name': 'Cambridge, MA'}
+    res_city_patch = requests.patch(city_url, data=head_city_patch)
+    j_city_patch = json.loads(res_city_patch.text)
+
+    assert res_city_patch.status_code == 200
+    assert j_city_patch['city_name'] == 'Cambridge, MA'
+    assert j_city_patch['city_name'] != 'Boston, MA'
+    assert j_city_patch['trip'] == j_trip_post['id']
+
+    # GET a city
+    res_city_get = requests.get(city_url)
+    j_city_get = json.loads(res_city_get.text)
+
+    assert res_city_get.status_code == 200
+    assert j_city_get['city_name'] == 'Cambridge, MA'
+    assert j_city_get['city_name'] != 'Boston, MA'
+    assert j_city_get['trip'] == j_trip_post['id']
+
+    # DELETE a city
+    res_delete_city = requests.delete(city_url)
+    assert res_delete_city.status_code == 204
+
+    # DELETE a trip
+    res_delete = requests.delete(trip_url)
     assert res_delete.status_code == 204
