3dc08af327abb6febec1cf28aa21a69f3da91810
Jonathan Frederick <jrfreder@gmail.com>
2015-11-05 17:16:15 -0500

    wrote interest and activity models

--- road_trip/trip/models.py
+++ road_trip/trip/models.py
@@ -23,3 +23,27 @@ class City(models.Model):
     lon = models.FloatField(null=True, blank=True)
     trip = models.ForeignKey(Trip)
     visited = models.BooleanField(default=False)
+
+
+class Activity(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField(null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    address = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    sub_category = models.CharField(max_length=255)
+    activity_stopover = models.BooleanField(default=False)
+    url = models.URLField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    img_url = models.URLField(max_length=255, null=True)
+    small_rate_img_url = models.URLField(max_length=255, null=True, blank=True)
+    large_rate_img_url = models.URLField(max_length=255, null=True, blank=True)
+    average_rating = models.FloatField(null=True, blank=True)
+    num_ratings = models.IntegerField(null=True, blank=True)
+    city = models.ForeignKey(City)
+
+
+class Interest(models.Model):
+    category = models.CharField(max_length=255)
+    sub_category = models.CharField(max_length=255)
+    trip = models.ForeignKey(Trip)
