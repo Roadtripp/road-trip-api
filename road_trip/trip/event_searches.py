77262399b1ce457e8be91665bba9ad4bf5cfea54
apierce1992 <apierce1992@gmail.com>
2015-11-07 13:14:33 -0500

    seatgeek is pretty much good

--- road_trip/trip/event_searches.py
+++ road_trip/trip/event_searches.py
@@ -190,92 +190,45 @@ def search_seatgeek(trip_id, performer, category, city):
     slug = performer.lower().replace(' ', '-')
     performer_data = requests.get("http://api.seatgeek.com/2/performers?slug={}".format(slug))
     performer_json = performer_data.json()
-    if category == "Performers":
+    print(performer_json)
+    try:
         performer_id = performer_json["performers"][0]["id"]
-        r = requests.get('http://api.seatgeek.com/2/recommendations?performers.id={id}&datetime_local.gte={start}&datetime_local.lt={end}&range=50mi&lat={lat}&lon={lon}&client_id={key}'.format(id=performer_id, start=str(trip.origin_date), end = str(trip.destination_date),lat = lat, lon = lon, key=SEAT_GEEK))
-
+        r = requests.get('http://api.seatgeek.com/2/recommendations?performers.id={id}&datetime_local.gte={start}&datetime_local.lt={end}&range=300mi&lat={lat}&lon={lon}&client_id={key}'.format(id=performer_id, start=str(trip.origin_date), end = str(trip.destination_date),lat = lat, lon = lon, key=SEAT_GEEK))
         parsed_json = r.json()
-        recs = []
-        counter = 0
-    #print(parsed_json)
-        try:
-            for rec in parsed_json["recommendations"]:
-                rec_dict = {}
-                rec["category"]=category
-                rec["subcategory"] = performer
-                rec["title"] = parsed_json["recommendations"][counter]["event"]["title"]
-                rec["datetime"] = parsed_json["recommendations"][counter]["event"]["datetime_local"]
-                rec["url"] = parsed_json["recommendations"][counter]["event"]["url"]
-                rec["address"] = parsed_json["recommendations"][counter]["event"]["address"]+", " +parsed_json["recommendations"][counter]["event"]["extended_address"]
-                rec["lowest_price"] =parsed_json["recommendations"][counter]["event"]["stats"]["lowest_price"]
-                recs.append(rec_dict)
-            #print(recs)
-            return recs
-        except:
-            pass
-
-
-
-
-
-
-
-# def search_events(trip_id, city):
-#     yelp = OAuth1Session(CONSUMER_KEY,
-#                                 client_secret=CONSUMER_SECRET,
-#                                 resource_owner_key=TOKEN,
-#                                 resource_owner_secret=TOKEN_SECRET)
-#
-#
-#     trip = Trip.objects.get(pk=trip_id)
-#     #city_list = find_cities(trip.origin, trip.destination)
-#     interest_food_list = Interest.objects.filter(trip=trip, category="food").all()
-#     interest_food_list = [x.sub_category for x in interest_food_list]
-#     yelp_food_list = []
-#      for item in interest_food_list:
-#          item = yelp_food_alias[item]
-#          yelp_food_list.append(item)
-
-#     interest_activity_list = Interest.objects.filter(trip=trip, category="hotels").all()
-#     interest_activity_list = [x.sub_category for x in interest_activity_list]
-#     yelp_activity_list = []
-    # for item in interest_activity_list:
-    #     item = yelp_activity_alias[item]
-    #     yelp_activity_list.append(item)
-
-#     interest_hotels_list = Interest.objects.filter(trip=trip, category="activities").all()
-#     interest_hotels_list = [x.sub_category for x in interest_hotels_list]
-#     yelp_hotels_list = []
-    #   for item in interest_hotels_list:
-    #       item = yelp_hotels_alias[item]
-    #       yelp_hotels_list.append(item)
-
-#     yelp_activity_list = ','.join(yelp_activity_list)
-#     yelp_food_list = ','.join(yelp_food_list)
-#     yelp_hotels_list = ','.join(yelp_hotels_list)
-#
-#     city_events = []
-#     url_activity = 'https://api.yelp.com/v2/search/?location={}&sort=2&category_filter={}'.format(city, yelp_activity_list)
-#     url_food = 'https://api.yelp.com/v2/search/?location={}&sort=2&category_filter={}'.format(city, yelp_food_list)
-#     url_hotel = 'https://api.yelp.com/v2/search/?location={}&sort=2&category_filter={}'.format(city, yelp_hotels_list)
-#     urls = [url_activity, url_food, url_hotel]
-#     for url in urls:
-#         r = yelp.get(url)
-#         r = r.json()
-#         counter = 0
-#         for business in r["businesses"]:
-#             bus = {}
-#             bus["name"] = r['businesses'][counter]['name']
-#             bus["category"] = url.replace(r'url_', '')
-#             bus["subcategory"] = r["businesses"][counter]["categories"]
-#             bus["rating"] = r['businesses'][counter]['rating']
-#             bus["url"] = r['businesses'][counter]['url']
-#             bus["num_reviews"] = r['businesses'][counter]['review_count']
-#             bus["rating_img_url_small"] = r['businesses'][counter]['rating_img_url_small']
-#             bus["rating_img_url"] = r['businesses'][counter]['rating_img_url']
-#             bus["phone"] = r['businesses'][counter]['display_phone']
-#             bus["address"] = r['businesses'][counter]['location']["display_address"]
-#             bus["city"]=city
-#             city_events.append(bus)
-#             counter += 1
-#     return city_events[0]
+        print(parsed_json)
+        if category == "artist":
+            recs = []
+            counter = 0
+            try:
+                for rec in parsed_json["recommendations"]:
+                    rec_dict = {}
+                    rec["category"]=category
+                    rec["subcategory"] = performer
+                    rec["title"] = parsed_json["recommendations"][counter]["event"]["title"]
+                    rec["datetime"] = parsed_json["recommendations"][counter]["event"]["datetime_local"]
+                    rec["url"] = parsed_json["recommendations"][counter]["event"]["url"]
+                    rec["address"] = parsed_json["recommendations"][counter]["event"]["address"]+", " +parsed_json["recommendations"][counter]["event"]["extended_address"]
+                    rec["lowest_price"] =parsed_json["recommendations"][counter]["event"]["stats"]["lowest_price"]
+                    recs.append(rec_dict)
+                return recs
+            except:
+                pass
+        if category == "sports":
+            recs = []
+            counter = 0
+            try:
+                for rec in parsed_json["recommendations"]:
+                    rec_dict = {}
+                    rec["category"]=category
+                    rec["subcategory"] = performer
+                    rec["title"] = parsed_json["recommendations"][counter]["event"]["title"]
+                    rec["datetime"] = parsed_json["recommendations"][counter]["event"]["datetime_local"]
+                    rec["url"] = parsed_json["recommendations"][counter]["event"]["url"]
+                    rec["address"] = parsed_json["recommendations"][counter]["event"]["address"]+", " +parsed_json["recommendations"][counter]["event"]["extended_address"]
+                    rec["lowest_price"] =parsed_json["recommendations"][counter]["event"]["stats"]["lowest_price"]
+                    recs.append(rec_dict)
+                return recs
+            except:
+                pass
+    except:
+        pass
