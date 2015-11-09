1c83c5954946cdc9bb434c598800065ff1bf85e4
Jonathan Frederick <jrfreder@gmail.com>
2015-11-09 14:59:58 -0500

    forgot to change a couple "date"s to "datetimes"

--- road_trip/trip/serializers.py
+++ road_trip/trip/serializers.py
@@ -47,8 +47,8 @@ class TripSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Trip
-        fields = ('id', 'title', 'origin', 'origin_date', 'origin_time',
+        fields = ('id', 'title', 'origin', 'origin_datetime', 'origin_time',
                   'origin_lat', 'origin_lon', 'destination',
-                  'destination_date', 'destination_time', 'destination_lat',
+                  'destination_datetime', 'destination_time', 'destination_lat',
                   'destination_lon', 'cities')
         depth = 2
