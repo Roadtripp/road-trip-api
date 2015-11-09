c839c07aa735927a06898d71abf58619f3ac2548
Jonathan Frederick <jrfreder@gmail.com>
2015-11-09 13:11:00 -0500

    added rest framework token auth

--- road_trip/road_trip/urls.py
+++ road_trip/road_trip/urls.py
@@ -41,4 +41,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
+    url(r'^login/', 'rest_framework.authtoken.views.obtain_auth_token'),
+
 ]
