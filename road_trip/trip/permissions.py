1cac9d0997f781f5b2f9b6a2fe41642a1a12b4d1
Jonathan Frederick <jrfreder@gmail.com>
2015-11-13 16:06:16 -0500

    only owners can access ActivityViewSet, added owned_if_owner manual permissions for function based views

--- road_trip/trip/permissions.py
+++ road_trip/trip/permissions.py
@@ -17,3 +17,12 @@ class CityPermissions(permissions.BasePermission):
         if obj.user:
             return obj.trip.user == request.user
         return False
+
+
+class ActivityPermissions(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
+        if obj.user:
+            return obj.trip.user == request.user
+        return False
