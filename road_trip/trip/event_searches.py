2cb441bc3b501e56f98f9bbe86bdf8879fe956c6
apierce1992 <apierce1992@gmail.com>
2015-11-13 11:33:55 -0500

    took out commented code

--- road_trip/trip/event_searches.py
+++ road_trip/trip/event_searches.py
@@ -6,7 +6,6 @@ from .city_selector import *
 from .models import Trip
 from .models import Interest
 import re
-import requests_cache
 
 
 #OAuth credential placeholders that must be filled in by users.
@@ -15,8 +14,6 @@ CONSUMER_SECRET = os.environ["YELP_CONSUMER_SECRET"]
 TOKEN = os.environ["YELP_TOKEN"]
 TOKEN_SECRET = os.environ["YELP_TOKEN_SECRET"]
 SEAT_GEEK = os.environ["SEAT_GEEK_KEY"]
-
-#requests_cache.install_cache(backend="sqlite", expire_after=36000)
 
 
 yelp = OAuth1Session(CONSUMER_KEY,
