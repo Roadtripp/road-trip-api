854a5824b120dbdb86953d890976db6cb3f91a07
apierce1992 <apierce1992@gmail.com>
2015-11-14 14:43:37 -0500

    moved dictionaries to library file

--- road_trip/trip/event_searches.py
+++ road_trip/trip/event_searches.py
@@ -6,7 +6,7 @@ from .city_selector import *
 from .models import Trip
 from .models import Interest
 import re
-
+from .alias_lib import *
 
 #OAuth credential placeholders that must be filled in by users.
 CONSUMER_KEY = os.environ["YELP_CONSUMER"]
@@ -20,117 +20,6 @@ yelp = OAuth1Session(CONSUMER_KEY,
                             client_secret=CONSUMER_SECRET,
                             resource_owner_key=TOKEN,
                             resource_owner_secret=TOKEN_SECRET)
-
-
-yelp_food_alias = {
-
-"asianfusion":"asianfusion",
-"barbeque":"bbq",
-"burgers":"burgers",
-"cafes":"cafes",
-"chinese":"chinese",
-"comfortfood":"comfortfood",
-"filipino":"filipino",
-"fishandchips":"fishnchips",
-"foodstands":"foodstands",
-"gastropubs":"gastropubs",
-"glutenfree":"gluten_free",
-"italian":"italian",
-"mexican":"mexican",
-"americannew":"newamerican",
-"pizza":"pizza",
-"salad":"salad",
-"sandwiches":"sandwiches",
-"seafood":"seafood",
-"restaurants":"restaurants",
-"steakhouses":"steak",
-"thai":"thai",
-"americantraditional":"tradamerican",
-"vegan":"vegan"
-}
-
-
-yelp_activity_alias  = {
-
-"amusementparks":"amusementparks",
-"aquariums":"aquariums",
-"beaches":"beaches",
-"bikerentals":"bikerentals",
-"boating":"boating",
-"bowling":"bowling",
-"challengecourses":"challengecourses",
-"climbing":"climbing",
-"discgolf":"discgolf",
-"jetskis":"jetskis",
-"kidsactivities":"kids_activities",
-"parks":"parks",
-"publicplazas":"publicplazas",
-"waterparks":"waterparks",
-"zoos":"zoos",
-"nightlife":"nightlife",
-"bars":"bars",
-"museums":"museums",
-"sportsteams":"sportsteams",
-"wineries":"wineries",
-"shopping":"shoppingcenters,outlet_stores,souvenirs,deptstores",
-"tours":"tours"
-
-}
-
-yelp_hotels_alias = {
-
-"bedandbreakfast":"bedbreakfast",
-"campgrounds":"campgrounds",
-"healthretreats":"healthretreats",
-"hostels":"hostels",
-"hotels":"hotels",
-"resorts":"resorts",
-"rvparks":"rvparks",
-"skiresorts":"skiresorts"
-
-}
-
-sg_sports_alias = {
-"football": "2063",
-"soccer":"310281",
-"basketball": "2100",
-"golf": "5738",
-"baseball": "2",
-"hockey":"2129",
-"nascar":"5802",
-"boxing":"57701",
-"wrestling":"136010",
-"tennis":"5702",
-"nfl":"2063",
-"nhl":"2129",
-"mlb":"2",
-"mls":"310281",
-"nba":"2100",
-"wwe":"136010",
-}
-
-sg_artist_alias = {
-"comedy":"5172",
-"broadway":"296233",
-"country":"35",
-"rock":"1896",
-"rap":"1109",
-"hip-hop":"2351",
-"pop":"1173",
-"classic-rock":"93736",
-"punk":"147",
-"christian":"1913",
-"heavy-metal":"12845",
-"death-metal":"147969",
-"latin":"1531",
-"blues":"147120",
-"jazz":"8705",
-"reggae":"225970",
-"electronica":"19340",
-
-}
-
-
 
 
 
